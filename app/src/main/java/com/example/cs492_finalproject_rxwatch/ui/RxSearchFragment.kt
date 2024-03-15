f32a591411acd272137b542e6e899a602d3a6610
Seth Weiss <sweisss@gmail.com>
2024-03-15 12:49:56 -0700

    Add search box text entry to onClickListener

--- app/src/main/java/com/example/cs492_finalproject_rxwatch/ui/RxSearchFragment.kt
+++ app/src/main/java/com/example/cs492_finalproject_rxwatch/ui/RxSearchFragment.kt
@@ -1,8 +1,11 @@
 package com.example.cs492_finalproject_rxwatch.ui
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.cs492_finalproject_rxwatch.R
@@ -13,10 +16,15 @@ class RxSearchFragment : Fragment(R.layout.rx_search_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val searchBox: EditText = view.findViewById(R.id.et_search_box)
         searchButton = view.findViewById(R.id.btn_navigate)
 
         searchButton.setOnClickListener {
             val directions = RxSearchFragmentDirections.navigateToDrugReport()
+            val query = searchBox.text.toString()
+            if (!TextUtils.isEmpty(query)) {
+                Log.d("RxSearchFragment", "Search query: $query")
+            }
             findNavController().navigate(directions)
         }
 
