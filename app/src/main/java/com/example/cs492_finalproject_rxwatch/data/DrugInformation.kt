07ae8086c2cce6f6108e3052bfe3fd05c21bdc21
Seth Weiss <sweisss@gmail.com>
2024-03-15 13:57:08 -0700

    Initialize DrugInformation data class

--- app/src/main/java/com/example/cs492_finalproject_rxwatch/data/DrugInformation.kt
+++ app/src/main/java/com/example/cs492_finalproject_rxwatch/data/DrugInformation.kt
@@ -1,10 +1,27 @@
 package com.example.cs492_finalproject_rxwatch.data
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class DrugInformation (
-    val test: String
+    val meta: DrugLabelMeta,
+    val results: List<DrugWarnings>
+)
 
-    // TODO: Implement DrugInformation class
+@JsonClass(generateAdapter = true)
+data class DrugLabelMeta (
+    @Json(name = "last_updated") val lastUpdated: String,
+    val results: LabelCounts
+)
+
+@JsonClass(generateAdapter = true)
+data class LabelCounts (
+    val total: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class DrugWarnings (
+    val warnings: List<String>,
+    @Json(name = "do_not_use") val doNotUse: List<String>
 )
