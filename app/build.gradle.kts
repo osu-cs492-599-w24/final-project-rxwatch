1d87596686c6ca466f671d3d15ff2c45a4dcafad
Seth Weiss <sweisss@gmail.com>
2024-03-18 15:40:52 -0700

    Add OnConflictStrategy.REPLACE to SearchedDrugDAO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-common:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
     implementation("com.google.android.material:material:1.11.0")
