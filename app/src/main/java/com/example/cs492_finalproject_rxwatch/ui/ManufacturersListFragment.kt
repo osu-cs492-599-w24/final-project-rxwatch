da064f59490400db3a95b4b0473ed9b307a68b0a
Seth Weiss <sweisss@gmail.com>
2024-03-19 11:55:16 -0700

    Change manufacturers list layout to ExpandableList

---
+++ app/src/main/java/com/example/cs492_finalproject_rxwatch/ui/ManufacturersListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cs492_finalproject_rxwatch.ui
+
+import androidx.fragment.app.Fragment
+import com.example.cs492_finalproject_rxwatch.R
+
+class ManufacturersListFragment : Fragment(R.layout.manufacturers_list) {
+
+}
